fee9e8a3301cfaede8d7d4dc804a3395c6b2a8a9
tomv-m <tom@merrienboer.eu>
2019-11-17 18:19:31 +0100

    including fonts

--- webpack.common.js
+++ webpack.common.js
@@ -50,13 +50,26 @@ module.exports = {
             },
           },
         ],
-      }
+      },
+      {
+        test: /\.(woff|woff2|ttf|otf|eot)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts',
+            },
+          },
+        ],
+      },
     ]
   },
   plugins: [
     new CopyPlugin([
       { from: './public', to: finalPath, force: true },
-      { from: './src/img', to: path.join(finalPath, '/img'), force: true },
+      { from: './src/fonts', to: path.join(finalPath, '/fonts'), force: true },
+      { from: './src/images', to: path.join(finalPath, '/images'), force: true },
     ]),
     new MiniCssExtractPlugin({
       filename: 'style.css',
